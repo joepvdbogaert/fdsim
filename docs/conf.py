7806ac9783ec08367a4dbeb5e99eb30c6f68b2fc
Bogaert <j.v.d.bogaert@tue.nl>
2019-03-05 12:23:37 +0100

    Update docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'fdsim'
-copyright = '2018, Joep van den Bogaert'
+copyright = '2019, Joep van den Bogaert'
 author = 'Joep van den Bogaert'
 
 # The short X.Y version
@@ -43,7 +43,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'numpydoc'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -79,7 +79,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/index.rst
+++ docs/index.rst
@@ -1,14 +1,14 @@
 fdsim documentation
 ===================
 
-.. mdinclude:: ../../README.md
+.. mdinclude:: ../README.md
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
    Simulation <simulation>
-   Fitting distributions to data <fitting>
+   Fitting distributions <fitting>
    Sampling <sampling>
    Dispatching <dispatching>
    Prediction <predictors>
