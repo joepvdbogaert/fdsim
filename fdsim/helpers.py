18c69ab8748fa5f6cbabde72b5d11fa040e6e9de
Bogaert <j.v.d.bogaert@tue.nl>
2019-07-17 09:58:34 +0200

    add function to create service area dictionary from kvt data

--- fdsim/helpers.py
+++ fdsim/helpers.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import pickle
 import time
 
@@ -65,3 +67,44 @@ def progress(text, verbose=True, same_line=False, newline_end=True):
     if verbose:
         print("{}[{}] {}".format("\r" if same_line else "", time.strftime("%Y-%m-%d %H:%M:%S"),
                                  text), end="\n" if newline_end else "")
+
+
+def create_service_area_dict(kvt_path, veh_col="vehicle_type", station_col="kazerne_naam",
+                             loc_col="vak_nr", station_filter=None):
+    """Create a dictionary of service areas based on KVT data.
+
+    Parameters
+    ----------
+    kvt_path: str
+        The path to the KVT data.
+    veh_col, station_col, loc_col: str
+        Column names that refer to the vehicle type, station name, and location id
+        respectively.
+    station_filter: array-like of strings, default=None
+        Stations to use. If None, use all. Will be converted to upper case.
+
+    Returns
+    -------
+    service_areas: dict
+        A dictionary like {'vehicle type' -> {'station name' -> [loc1, loc2, ...]}}.
+    """
+    data = pd.read_csv(kvt_path)
+    if station_filter is not None:
+        data = data[np.in1d(data['kazerne_naam'], pd.Series(station_filter).str.upper())]
+
+    grouped = (data.groupby([veh_col, station_col])[loc_col]
+                   .apply(lambda x: x.tolist())
+                   .reset_index(veh_col)
+                   .pivot(columns=veh_col)
+              )
+    grouped.columns = grouped.columns.droplevel(0)
+
+    d = grouped.to_dict()
+
+    # remove NaNs from dictionary
+    for v in data[veh_col].unique():
+        for s in data[station_col].unique():
+            if np.isnan(d[v][s]).all():
+                del d[v][s]
+
+    return d
